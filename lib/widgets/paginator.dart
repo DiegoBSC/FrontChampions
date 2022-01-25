115ff27fa11cc1051f0f086aaf10d63730dea616
Diego Acosta <diego1503bsc@gmail.com>
2022-01-25 01:47:57 -0500

    Paginador de Usuarios

---
+++ lib/widgets/paginator.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:sport_system_play_mono/constants.dart';
+
+class Paginator extends StatefulWidget {
+  int page;
+  int size;
+  int totalPage;
+  Function(int)? onChangePage;
+
+  Paginator(
+      {Key? key,
+      required this.page,
+      required this.size,
+      this.onChangePage,
+      required this.totalPage})
+      : super(key: key);
+
+  @override
+  State<Paginator> createState() => _PaginatorState();
+}
+
+class _PaginatorState extends State<Paginator> {
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          decoration: BoxDecoration(
+            color: primaryColor,
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+          height: 40,
+          child: IconButton(
+            icon: const Icon(Icons.arrow_back_ios),
+            tooltip: 'Atras',
+            onPressed: () {
+              setState(() {
+                if (widget.page > 0) {
+                  widget.page -= 1;
+                  widget.onChangePage!(widget.page);
+                }
+              });
+            },
+          ),
+        ),
+        SizedBox(
+          width: 10,
+        ),
+        Text(
+            widget.totalPage.toString() + ' / ' + (widget.page + 1).toString()),
+        SizedBox(
+          width: 10,
+        ),
+        Container(
+          decoration: BoxDecoration(
+            color: primaryColor,
+            borderRadius: BorderRadius.all(
+              Radius.circular(10),
+            ),
+          ),
+          height: 40,
+          child: IconButton(
+            icon: const Icon(Icons.arrow_forward_ios),
+            tooltip: 'Adelante',
+            onPressed: () {
+              setState(() {
+                if (widget.page < widget.totalPage - 1) {
+                  widget.page += 1;
+                  widget.onChangePage!(widget.page);
+                }
+              });
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
--- lib/widgets/recent_files.dart
+++ lib/widgets/recent_files.dart
@@ -23,27 +23,108 @@ class RecentFiles extends StatelessWidget {
             "Ultimas Reservas",
             style: Theme.of(context).textTheme.subtitle1,
           ),
-          SizedBox(width: double.infinity, child: Text("tABLA")
-              // DataTable2(
-              //   columnSpacing: defaultPadding,
-              //   minWidth: 600,
-              //   columns: const [
-              //     DataColumn(
-              //       label: Text("Nro. Cancha"),
-              //     ),
-              //     DataColumn(
-              //       label: Text("Fecha"),
-              //     ),
-              //     DataColumn(
-              //       label: Text("Horario"),
-              //     ),
-              //   ],
-              // rows: List.generate(
-              // demoRecentFiles.length,
-              //   (index) => recentFileDataRow(demoRecentFiles[index]),
-              // ),
-              // ),
+          SizedBox(width: double.infinity, child: Text("tABLA")),
+
+          TextFormField(
+            initialValue: '',
+            maxLength: 20,
+            decoration: InputDecoration(
+                icon: Icon(
+                  Icons.favorite,
+                  color: primaryColor,
+                ),
+                labelText: 'Usuario',
+                labelStyle: TextStyle(
+                  color: primaryColor,
+                ),
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(color: primaryColor),
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(10)),
+                  borderSide: BorderSide(width: 1, color: primaryColor),
+                ),
+                focusColor: Colors.red),
+            focusNode: FocusNode(),
+          ),
+
+//Modelo
+          TextField(
+            decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Tell us about yourself',
+                helperText: 'Keep it short, this is just a demo.',
+                labelText: 'Life story',
+                prefixIcon: const Icon(
+                  Icons.person,
+                  color: Colors.green,
+                ),
+                prefixText: ' ',
+                suffixText: 'USD',
+                suffixStyle: const TextStyle(color: Colors.green)),
+          ),
+//Modelo
+          Theme(
+            data: ThemeData(
+                primaryColor: primaryColor, primaryColorDark: primaryColor),
+            child: TextField(
+              decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(10)),
+                    borderSide: BorderSide(width: 1, color: primaryColor),
+                  ),
+                  border: OutlineInputBorder(),
+                  hintText: 'Digita nombre de usuario',
+                  labelText: 'Nombre Usuario',
+                  prefixIcon: const Icon(
+                    Icons.person,
+                    color: primaryColor,
+                  ),
+                  suffixStyle: const TextStyle(color: Colors.green)),
+            ),
+          ),
+          SizedBox(
+            height: 50,
+          ),
+
+          TextFormField(
+            decoration: InputDecoration(
+              fillColor: bgColor,
+              filled: true,
+              hintText: "Usuario",
+              labelText: 'Usuario',
+              labelStyle: TextStyle(
+                color: primaryColor,
               ),
+              focusedBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                borderSide: BorderSide(width: 1, color: primaryColor),
+              ),
+              enabledBorder: OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(10)),
+                borderSide: BorderSide(color: primaryColor),
+              ),
+            ),
+          )
+          // DataTable2(
+          //   columnSpacing: defaultPadding,
+          //   minWidth: 600,
+          //   columns: const [
+          //     DataColumn(
+          //       label: Text("Nro. Cancha"),
+          //     ),
+          //     DataColumn(
+          //       label: Text("Fecha"),
+          //     ),
+          //     DataColumn(
+          //       label: Text("Horario"),
+          //     ),
+          //   ],
+          // rows: List.generate(
+          // demoRecentFiles.length,
+          //   (index) => recentFileDataRow(demoRecentFiles[index]),
+          // ),
+          // ),
         ],
       ),
     );
